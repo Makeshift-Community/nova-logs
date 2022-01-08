93ddf18a45a5417fd8a2b97b3bc5801804028f4b
NKN1396 <someone.took.nkn1396@gmail.com>
2022-01-08 07:00:49 +0100

    Lint

--- src/functions/notifyOwner.js
+++ src/functions/notifyOwner.js
@@ -1,6 +1,6 @@
 import { owner as ownerId } from '../resources/makeshift.js'
 
-export default async function(client) {
-  let nkn1396 = await client.users.fetch(ownerId)
-  nkn1396.send("👁 nova-logs has been started")
+export default async function (client) {
+  const nkn1396 = await client.users.fetch(ownerId)
+  nkn1396.send('👁 nova-logs has been started')
 }
