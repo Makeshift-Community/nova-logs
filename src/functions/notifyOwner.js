b48b0c77f90eec1c289b0f865a8d9f3caf67ce98
NKN1396 <someone.took.nkn1396@gmail.com>
2022-01-08 09:54:17 +0100

    Fix missing error handling for notifyOwner.js

--- src/functions/notifyOwner.js
+++ src/functions/notifyOwner.js
@@ -2,5 +2,10 @@ import { owner as ownerId } from '../resources/makeshift.js'
 
 export default async function (client) {
   const nkn1396 = await client.users.fetch(ownerId)
-  nkn1396.send('👁 nova-logs has been started')
+    .catch(console.error)
+  if (nkn1396 === undefined) {
+    console.error('Could not announce startup to owner.')
+    return
+  }
+  return nkn1396.send('👁 nova-logs has been started')
 }
