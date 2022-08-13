2f3355ea42d7e12c5840df8f81e4539eb47bb616
NKN1396 <someone.took.nkn1396@gmail.com>
2022-08-13 15:03:59 +0200

    Type fetchGuild

--- src/functions/fetchGuild.ts
+++ src/functions/fetchGuild.ts
@@ -1,6 +1,7 @@
+import { Client } from 'discord.js'
 import { guild as guildId } from '../resources/makeshift.js'
 
-export default async function (client) {
+export default async function (client : Client) {
   const makeshift = await client.guilds.fetch(guildId).catch(console.error)
   if (makeshift === undefined) {
     console.error('Could not fetch Makeshift guild.')
