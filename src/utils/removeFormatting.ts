01c957e5e32da9c2cac03a840b6b9e24616038a6
NKN1396 <someone.took.nkn1396@gmail.com>
2022-08-13 14:53:22 +0200

    Type removeFormatting

--- src/utils/removeFormatting.ts
+++ src/utils/removeFormatting.ts
@@ -1,6 +1,6 @@
 const discordMarkdownRegex = /[\\*_~|`<>]/g
 
-export default function (text) {
+export default function (text : string) {
   return text.replace(discordMarkdownRegex, (match) => {
     return `\\${match}`
   })
