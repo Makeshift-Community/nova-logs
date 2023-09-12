de2d1ea8d00d5c8452eb817756c5d397b531c8ca
NKN1396 <someone.took.nkn1396@gmail.com>
2023-09-12 04:57:43 +0200

    style: Lint

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,9 +1,11 @@
 module.exports = {
-  apps : [{
-    name   : "nova-logs",
-    script : "dist/server.js",
-    env_production: {
-      NODE_ENV: "production",
+  apps: [
+    {
+      name: "nova-logs",
+      script: "dist/server.js",
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
-  }]
-}
+  ],
+};
