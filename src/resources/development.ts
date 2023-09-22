6987b41c5235e3ecd0745ff5461912b0e2a983a4
NKN1396 <someone.took.nkn1396@gmail.com>
2023-09-23 01:25:00 +0200

    fix: Remove channel const from configuration

--- src/resources/development.ts
+++ src/resources/development.ts
@@ -1,5 +1,5 @@
 export const guild = "299298454457548801";
-export const channel = "1008538549529878679";
+
 export const owner = "153595272465743872";
 
 export const enum Channels {
--- src/resources/makeshift.ts
+++ src/resources/makeshift.ts
@@ -1,5 +1,5 @@
 export const guild = "272036959348588555";
-export const channel = "303662183558938625";
+
 export const owner = "153595272465743872";
 
 export const enum Channels {
