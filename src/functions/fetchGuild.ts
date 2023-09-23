3bbbc21202b1de07025cd5c3b70cb4f24df0a8d2
NKN1396 <someone.took.nkn1396@gmail.com>
2023-09-23 02:12:41 +0200

    fix: Add error messages

--- src/functions/fetchGuild.ts
+++ src/functions/fetchGuild.ts
@@ -1,11 +1,15 @@
 import { Client } from "discord.js";
 import { GUILD as guildId } from "../resources/configuration.js";
 
-export default async function (client: Client): Promise<unknown> {
-  const makeshift = await client.guilds.fetch(guildId).catch(console.error);
-  if (makeshift === undefined) {
+export default async function (client: Client): Promise<void> {
+  const makeshift = await client.guilds.fetch(guildId).catch(()=>{
     console.error("Could not fetch Makeshift guild.");
+  });
+  if (makeshift === undefined) {
+    console.error("Makeshift guild is undefined.");
     return;
   }
-  return await makeshift.members.fetch();
+  makeshift.members.fetch().catch(()=>{
+    console.error("Could not fetch Makeshift guild members.");
+  });
 }
