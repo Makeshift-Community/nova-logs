8c614e254e3e04a510d34b0524f2f01b473b15b0
NKN1396 <someone.took.nkn1396@gmail.com>
2023-09-23 02:16:31 +0200

    style: Lint

--- src/functions/fetchGuild.ts
+++ src/functions/fetchGuild.ts
@@ -2,14 +2,14 @@ import { Client } from "discord.js";
 import { GUILD as guildId } from "../resources/configuration.js";
 
 export default async function (client: Client): Promise<void> {
-  const makeshift = await client.guilds.fetch(guildId).catch(()=>{
+  const makeshift = await client.guilds.fetch(guildId).catch(() => {
     console.error("Could not fetch Makeshift guild.");
   });
   if (makeshift === undefined) {
     console.error("Makeshift guild is undefined.");
     return;
   }
-  makeshift.members.fetch().catch(()=>{
+  makeshift.members.fetch().catch(() => {
     console.error("Could not fetch Makeshift guild members.");
   });
 }
