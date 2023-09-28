36395424060a61bf2c999f0f144c90de4119791c
NKN1396 <someone.took.nkn1396@gmail.com>
2023-09-28 03:01:26 +0200

    fix: Reinstate old markdown sequence escaper

--- src/utils/escapeMarkdown.ts
+++ src/utils/escapeMarkdown.ts
@@ -1,11 +1,5 @@
-import { escapeMarkdown, EscapeMarkdownOptions } from "discord.js";
-
 export default function (text: string): string {
-  const options: EscapeMarkdownOptions = {
-    bulletedList: true,
-    heading: true,
-    maskedLink: true,
-    numberedList: true,
-  };
-  return escapeMarkdown(text, options);
+  const discordMarkdownRegex = /[\\*_~|`<#\-[.]/g;
+
+  return text.replaceAll(discordMarkdownRegex, `\\$&`);
 }
