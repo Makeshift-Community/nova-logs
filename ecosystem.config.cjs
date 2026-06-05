87c10f5d3baf53451d905d06e1707ee7c2f91dbc
NKN1396 <someone.took.nkn1396@gmail.com>
2026-06-06 01:26:49 +0200

    fix: Wrong script in ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "nova-logs",
-      script: "dist/server.js",
+      script: "dist/bot.js",
       env_production: {
         NODE_ENV: "production",
       },
